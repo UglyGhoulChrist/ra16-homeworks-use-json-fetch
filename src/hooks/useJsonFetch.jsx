33e49a2ff1e06ddc2141a89e33bffcdab3892cd7
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2023-02-11 16:45:03 +0300

    v1.0.0

--- src/hooks/useJsonFetch.jsx
+++ src/hooks/useJsonFetch.jsx
@@ -2,33 +2,17 @@ import { useEffect, useState } from "react";
 
 const useJsonFetch = function (url) {
   const [data, setData] = useState("");
-  const [error, setError] = useState("false");
+  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
     setTimeout(() => {
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", url);
-      xhr.send();
-      xhr.onload = function () {
-        // Проверка что код сервера 2xx
-        if (~~(xhr.status / 100) === 2) {
-          // Проверка на JSON
-          try {
-            const data = JSON.parse(xhr.responseText);
-            setData(data);
-          } catch (e) {
-            setError(error);
-          }
-        } else {
-          setError(error);
-        }
-        setLoading(false);
-      };
-      xhr.onerror = function () {
-        setError(error);
-      };
-    }, 2000);
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .catch((error) => setError(error))
+        .finally(() => setLoading(false));
+    }, 3000);
   }, []);
   return [data, error, loading];
 };
